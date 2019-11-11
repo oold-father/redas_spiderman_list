2bd2dde2f8c9eb6a5c662dd53baf9e4159ad748a
Yinxin <yinxin666ccyy@gmail.com>
2019-11-11 15:32:11 +0800

    feat(spider): 添加智联招聘爬虫

---
+++ modules/spider_zhipin.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/11/11 下午1:57
+# @Author  : yinxin
+# @File    : spider_zhipin
+# @Software: PyCharm
+
+from modules import Spider
+from common import hlog
+from utils import crawl_url,send_data
+import time
+
+class SpiderZhipin(Spider):
+
+    def __init__(self,url,num):
+
+        self.start_url = url.split("?")[0]
+        self.max_page = num
+
+        hlog.var("SpiderZhipin.start_url", self.start_url)
+        hlog.var("SpiderZhipin.max_page", self.max_page)
+
+    def handle(self):
+        url_list = [self.start_url + "?page=%s&ka=page-%s"%(i,i) for i in range(1,self.max_page+1)]
+
+        for url in url_list:
+            html = crawl_url(url)
+            send_data(url, html)
+
+        time.sleep(3)
+
+
