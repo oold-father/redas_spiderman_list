147269e3fb2ddda745fd73da9906abf20ab508c5
Yinxin <yinxin666ccyy@gmail.com>
2019-11-11 13:51:39 +0800

    feat(入口):添加爬虫入口

---
+++ application.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/11/11 下午1:50
+# @Author  : yinxin
+# @File    : application
+# @Software: PyCharm
+
+import time
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    while True:
+        main()
+        time.sleep(5)
